153a44989771944b451cbdb82daf1af80558ca84
Majdi-si <91064376+Majdi-si@users.noreply.github.com>
2024-09-16 07:42:08 +0000

    Add OCaml sandbox configuration and implement basic expression evaluation functions

---
+++ SITA23/OCAML/cours.ml
@@ -0,0 +1,49 @@
+type expr =
+  | False
+  | True
+  | Var of string
+  | Or of expr * expr
+  | And of expr * expr
+
+let rec nb_nodes = fun e ->
+  match e with
+  | False -> 1
+  | True -> 1
+  | Var _ -> 1
+  | Or (e1, e2) -> 1 + nb_nodes e1 + nb_nodes e2
+  | And (e1, e2) -> 1 + nb_nodes e1 + nb_nodes e2
+
+let rec depth = fun e -> 
+    match e with 
+    | False -> 1
+    | True -> 1
+    | Var _ -> 1
+    | Or (e1, e2) -> 1 + max (depth e1) (depth e2)
+    | And (e1, e2) -> 1 + max (depth e1) (depth e2)
+
+let rec nb_var = fun e ->
+    match e with
+    | False -> 0
+    | True -> 0
+    | Var _ -> 1
+    | Or (e1, e2) -> 1 + nb_var(e1) + nb_var(e2)
+    | And (e1, e2) -> 1 + nb_var(e1) + nb_var(e2)
+
+let rec eval = fun e ex ->
+    match e with
+    | False -> false
+    | True -> true
+    | Var x -> failwith "Variable not found"
+    | Or (e1, e2) -> (eval e1 ex) || (eval e2 ex)
+    | And (e1, e2) -> (eval e1 ex) && (eval e2 ex)
+
+
+
+let () =
+    let expr1 = Or (And (Var "x", Var "y"), Or (Var "z", False)) in
+    Printf.printf "nb_nodes(expr1) = %d\n" (nb_nodes expr1);
+    Printf.printf "depth(expr1) = %d\n" (depth expr1);
+    Printf.printf "nb_var(expr1) = %d\n" (nb_var expr1);
+    Printf.printf "eval(expr1, {x=true, y=false, z=true}) = %b\n" (eval expr1 [("x", true); ("y", false); ("z", true)]);
+    Printf.printf "eval(expr1, {x=true, y=false, z=false}) = %b\n" (eval expr1 [("x", true); ("y", false); ("z", false)]);
+    
