76e90523797bde0267d32f60a00a77f642f82af9
Majdi-si <91064376+Majdi-si@users.noreply.github.com>
2024-09-22 22:11:35 +0000

    Refactoriser l'initialisation du pointeur de pile et la boucle de multiplication

--- SITA23/SPP/td2_2.asm
+++ SITA23/SPP/td2_2.asm
@@ -12,27 +12,35 @@ val_debut equ 0
     resultat3: var 1?
 
     org 20
+    stack_base: var 0?      ; Simuler la pile avec une adresse de base (ajoutez une ligne pour la pile)
 
-debut: ldr r1, [nombre1] ; multiplication de nombre1 par mult
+    org 40                   ; Assurez-vous d'avoir une zone de mémoire suffisante
+
+debut: 
+    ldr r3, [stack_base] ; Initialiser l'adresse de base de la pile dans r3
+    ldr r1, [nombre1]      ; multiplication de nombre1 par mult
     ldr r2, [mult]
     call multiplication
     str [resultat1], r0
 
-    ldr r1, [nombre2] ; multiplication de nombre2 par mult
+    ldr r1, [nombre2]      ; multiplication de nombre2 par mult
     ldr r2, [mult]
     call multiplication
     str [resultat2], r0
 
-    ldr r1, [nombre3] ;
+    ldr r1, [nombre3]      ; multiplication de nombre3 par mult
     ldr r2, [mult]
     call multiplication
     str [resultat3], r0 
 
-multiplication: push r4
-    ldr r0, 0     
-boucle: add r0, r1r0 
-    dec r2  
-        jnz boucle   
-    pop r4  
-    ret
-;en lancant le programme instruction par instruction j'ai : RI = C254, R2 = 0001, R1 = 0004 et R4 = FFFF et dans décodeur : push r4
+    hlt                    ; Fin du programme
+
+multiplication: 
+    push r4                ; Sauvegarde de r4 dans la pile
+    ldr r0, 0              ; Initialisation de r0 à 0
+boucle: 
+    add r0, r1r0          ; Ajouter r1 à r0
+    dec r2                ; Décrémenter r2
+    jnz boucle            ; Reboucler tant que r2 n'est pas 0
+    pop r4                ; Restaurer r4 depuis la pile
+    ret                    ; Retour à l'appelant
