663f7cf7ac2b953e5b43ccfcd875b3be763b4bbb
Majdi-si <majdi.si.2003@gmail.com>
2024-09-26 12:10:17 +0200

    Refactoriser l'évaluation des variables et ajouter une nouvelle fonction pour évaluer les variables selon un prédicat donné dans le code

--- SITA23/OCAML/Cours/Tp/TP2/tp2.ml
+++ SITA23/OCAML/Cours/Tp/TP2/tp2.ml
@@ -38,8 +38,15 @@ let sigma_terminal_binaire f a b s cont binaire neutre =
   aux a neutre
 
 let integrale f a b dx = 
-  sigma_terminal_binaire f a b (fun x -> dx *. f x) (fun x -> x>b) (fun x y-> x +. y) 0.
+  sigma_terminal_binaire f a b (fun x -> x +. dx) (fun x -> x >= b) (fun x y -> x +. y) 0.
 
+let pi epsilon =
+  let f n = if n mod 2 = 0 then 1.0 /. (2.0 *. float_of_int n +. 1.0) else -.1.0 /. (2.0 *. float_of_int n +. 1.0) in
+  let s n = n + 1 in
+  let cont n = abs_float (f n) < epsilon in
+  let binaire (acc, n) term = (acc +. term, n + 1) in
+  let (approx, _) = sigma_terminal_binaire (fun n -> f n) 0 max_int s cont (fun (acc, n) term -> (acc +. term, n + 1)) (0.0, 0) in
+  4.0 *. approx
 
 let () =
   Printf.printf "sigma_direct (fun x -> x) 1 10 = %d\n" (sigma_direct (fun x -> x) 1 10);
@@ -57,4 +64,6 @@ let () =
   Printf.printf "sigma_terminal_predicat (fun x -> x) 1 10 (fun x -> x + 1) (fun x -> x > 10) = %d\n" (sigma_terminal_predicat (fun x -> x) 1 10 (fun x -> x + 1) (fun x -> x > 10));
   Printf.printf "sigma_terminal_binaire (fun x -> x) 1 10 (fun x -> x + 1) (fun x -> x > 10) (fun x y  -> x+y) = %d\n" (sigma_terminal_binaire (fun x -> x) 1 10 (fun x -> x + 1) (fun x -> x > 10) (fun x y -> x+y) 0);
   Printf.printf "Factoriel 5 = %d\n" (sigma_terminal_binaire (fun x -> x) 1 5 (fun x -> x+1) (fun x -> x > 5) (fun x y  -> x*y) 1);
-  Printf.printf "Intégrale de 1 à 100 de x² = %f\n" (integrale (fun x -> x*.x) 1. 100. 0.1);
+  Printf.printf "Intégrale de 1 à 100 de x² = %f\n" (integrale (fun x -> x *. x) 1. 100. 0.1);
+  let epsilon = 1e-6 in
+  Printf.printf "Approximation de π avec epsilon = %f : %f\n" epsilon (pi epsilon)
