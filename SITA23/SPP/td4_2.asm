aeccda35f2b0aaea1c63e70b78e3cd487a284234
Majdi-si <majdi.si.2003@gmail.com>
2024-09-26 14:56:09 +0200

    Refactoriser l'évaluation des variables et ajouter une nouvelle fonction pour évaluer les variables selon un prédicat donné dans le code

--- SITA23/SPP/td4_2.asm
+++ SITA23/SPP/td4_2.asm
@@ -68,6 +68,8 @@ org 40
 
 ; sous-programmes d'interruption
 It_man: push r0 ; sauvegarde du contexte (registre à modifier)
+    ldr r0, 1          ; init PICU (mode) qui permet de réactiver les interruptions (dans le cas d'une interruption manuelle)
+    ldr fl, r0        ; init PICU (mode) qui permet de réactiver les interruptions (dans le cas d'une interruption manuelle)
     ldr r0,0 ;remise à 0 de valeur (par l'intermédiaire d'un registre)
     str [valeur],r0 ; valeur = 0
     ldr r0,0 ; envoi du signal de fin d'interruption (EOI)
@@ -75,21 +77,28 @@ It_man: push r0 ; sauvegarde du contexte (registre à modifier)
     pop r0 ; restitution du contexte (registre modifié)
     iret ; retour au programme principal
 
-It_clav: 
+It_clav: push r0
+
+    pop r0
+    iret
 
 ; programme principal
-init: ldr r0, 0          ; CLI
+init: ldr r0, 0          ; init des flags
     ldr fl, r0         ; init des flags
     ldr r0, fond_pile  ; init SP
     ldr sp, r0       ; init SP : pile vide
     ldr r0,It_man ;initiation de l'adresse de l'it manuelle 
     str [ad_SPIt],r0 ; met l'adresse de l'it manuelle dans le TdV (table des vecteurs)
+    ldr r0, It_clav
+    str [ad_SPIclav], r0
     ldr r0, 1          ; init Port Parallèle
     str [PPMode], r0
     ldr r0, masque     ; init PICU (masque)
     str [PicuMask], r0
-    ldr r0, 1          ; STI
-    ldr fl, r0
+    ldr r0, 7
+    str [SModeEtat], r0
+    ldr r0, 1          ; init PICU (mode) qui permet de réactiver les interruptions (dans le cas d'une interruption manuelle)
+    ldr fl, r0        ; init PICU (mode) qui permet de réactiver les interruptions (dans le cas d'une interruption manuelle)
     ldr r0, 0xff       ; valeur max de valeur
     ldr r1, r0         ; stockée dans R1
     ldr r0, 0          ; init valeur à zero
@@ -100,6 +109,5 @@ bcle: ldr r0, [valeur]
     inc r0
     str [valeur], r0
     sub r4, r0r1
-    jnz bcle
     jmp bcle
     hlt
