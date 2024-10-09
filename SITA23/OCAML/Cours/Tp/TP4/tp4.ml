e4afe2ab7738e6b368e8206004b826db9cbd0212
Majdi-si <majdi.si.2003@gmail.com>
2024-10-09 14:09:57 +0200

    Refactoriser les fonctions de simplification et de dérivation pour améliorer l'affichage de l'expression

--- SITA23/OCAML/Cours/Tp/TP4/tp4.ml
+++ SITA23/OCAML/Cours/Tp/TP4/tp4.ml
@@ -1,32 +1,36 @@
-type 'a t = 
-  | Entree of 'a list
-  | Sortie of 'a list;;
+type 'a t = 'a list * 'a list;;
 
 
 exception FileVide;;
 
-let empty = Entree [];;
+let empty = ([],[]);;
 
 let is_empty = function
-  | Entree [] -> true
+  | ([], []) -> true
   | _ -> false;;
 
+let add = fun x t ->
+  match t with
+  | (l1, l2) -> (x::l1, l2);;
 
-let add e = function
-  | Entree l -> Entree (e :: l)
-  | Sortie l -> Entree (e :: l);;
 
-let take = function
-  | Entree [] -> raise FileVide
-  | Entree (e :: l) -> (e, Entree l)
-  | Sortie (e :: l) -> (e, Sortie l);;
+let take = fun t ->
+  match t with
+  | ([],_) -> raise FileVide
+  | (l1,[]) -> let l2 = List.rev l1 in (List.hd l2, ([],List.tl l2))
+  | (l1,l2) -> (List.hd l2, (l1,(List.tl l2)));;
+
+let print_tuple = fun t ->
+  match t with
+  | (l1,l2) -> Printf.printf "l1 = %s\nl2 = %s\n" (String.concat " " (List.map string_of_int l1)) (String.concat " " (List.map string_of_int l2));;
+
 
 let () = 
-  let t = empty in
-  Printf.printf "%b\n" (is_empty t);
-  let t = add 1 t in
-  Printf.printf "%b\n" (is_empty t);
+  let t = add 1 empty in
   let t = add 2 t in
   let t = add 3 t in
+  let t = add 4 t in
+  let t = add 5 t in
   let (e, t) = take t in
-  Printf.printf "%d\n" e;
+  Printf.printf "e = %d\n" e;
+  print_tuple t;;
