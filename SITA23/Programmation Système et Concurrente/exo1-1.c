9d520b83002a902a457af1e97d88549b334bf9d4
Majdi-si <majdi.si.2003@gmail.com>
2024-10-17 12:04:58 +0200

    Refactoriser le fichier exo1-1.c pour modifier les permissions lors de l'ouverture du fichier toto1-1.txt.

--- SITA23/Programmation Système et Concurrente/exo1-1.c
+++ SITA23/Programmation Système et Concurrente/exo1-1.c
@@ -4,13 +4,13 @@
 #include <stdio.h>   // pour perror
 
 int main(void) {
-    int fd, nbEcr, taille;
+    int fd, nbEcr, taille, nbRead;
 
     /* Ouverture du fichier toto.txt en écriture.
        - Le fichier est créé s'il n'existe pas (flag O_CREAT).
        - Le curseur de position courante dans le fichier est placé à la fin (flag O_APPEND). */
 
-    fd = open("toto1-1.txt", O_WRONLY | O_CREAT | O_APPEND, 0600);
+    fd = open("toto1-1.txt", O_RDWR | O_CREAT | O_APPEND, 0600);
     if (fd == -1) {
         perror("Erreur lors de l'ouverture du fichier");
         return 1;
@@ -21,6 +21,24 @@ int main(void) {
     nbEcr = write(fd, " Bonjour !\n", taille);
     if (nbEcr == -1) {
         perror("Erreur lors de l'écriture dans le fichier");
+        return 1;
+    }
+
+// Remettre le curseur au début du fichier
+    if (lseek(fd, 0, SEEK_SET) == -1) {
+        perror("Erreur lors du repositionnement du curseur");
+        close(fd);
+        return 1;
+    }
+
+    // Lire le contenu du fichier et l'afficher sur la sortie standard
+    char buffer[1024];
+    while ((nbRead = read(fd, buffer, sizeof(buffer))) > 0) {
+        write(1, buffer, nbRead);
+    }
+
+    if (nbRead == -1) {
+        perror("Erreur lors de la lecture du fichier");
         close(fd);
         return 1;
     }
