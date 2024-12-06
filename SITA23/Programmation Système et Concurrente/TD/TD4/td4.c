b208f899c5eaea4f6df4dbc7302fe755cba49f75
Majdi-si <majdi.si.2003@gmail.com>
2024-12-06 11:28:48 +0100

    Corriger les chemins d'exécution dans le programme pour fork et execl

--- SITA23/Programmation Système et Concurrente/TD/TD4/td4.c
+++ SITA23/Programmation Système et Concurrente/TD/TD4/td4.c
@@ -1,20 +1,21 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
-#include <fcntl.h>  
+#include <fcntl.h>
+#include <sys/wait.h>
 
 int main(void){
     int pid1, pid2, status;
     if((pid1 = fork()) == 0){
         printf("Je suis le fils 1, mon pid est %d et celui de mon père est %d\n", getpid(), getppid());
-        execl("Programmation Système et Concurrente/TD/TD4", "exexution1", NULL);
+        execl("./execution1", "execution1", NULL);
         perror("Erreur execl");
         sleep(1);
         exit(0);
     }
     if ((pid2 = fork()) == 0){
         printf("Je suis le fils 2, mon pid est %d et celui de mon père est %d\n", getpid(), getppid());
-        execl("Programmation Système et Concurrente/TD/TD4", "exexution2", NULL);
+        execl("./execution2", "execution2", NULL);
         perror("Erreur execl");
         sleep(1);
         exit(0);
