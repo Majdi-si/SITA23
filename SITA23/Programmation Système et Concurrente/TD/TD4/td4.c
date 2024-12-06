949134fc8859140291fb0c65eee7335f33e9906e
Majdi-si <majdi.si.2003@gmail.com>
2024-12-06 11:31:56 +0100

    Supprimer les pauses inutiles après les appels execl dans le programme td4.c

--- SITA23/Programmation Système et Concurrente/TD/TD4/td4.c
+++ SITA23/Programmation Système et Concurrente/TD/TD4/td4.c
@@ -10,14 +10,12 @@ int main(void){
         printf("Je suis le fils 1, mon pid est %d et celui de mon père est %d\n", getpid(), getppid());
         execl("./execution1", "execution1", NULL);
         perror("Erreur execl");
-        sleep(1);
         exit(0);
     }
     if ((pid2 = fork()) == 0){
         printf("Je suis le fils 2, mon pid est %d et celui de mon père est %d\n", getpid(), getppid());
         execl("./execution2", "execution2", NULL);
         perror("Erreur execl");
-        sleep(1);
         exit(0);
     }
     wait(&status);
